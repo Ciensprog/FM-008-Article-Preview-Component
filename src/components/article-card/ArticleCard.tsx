41f53ce5b799c57cb64b4d4acd467f254a0715e2
Ciensprog <jorg.leonrd@gmail.com>
2023-07-09 09:26:52 -0600

    fix: add z-index

--- src/components/article-card/ArticleCard.tsx
+++ src/components/article-card/ArticleCard.tsx
@@ -10,7 +10,7 @@ import { ArticleCardInformation } from './ArticleCardInformation'
 
 export function ArticleCard() {
   return (
-    <article className="bg-white max-w-[20.4375rem] rounded-[0.625rem] shadow-[0_40px_40px_-10px_rgba(201_213_225_/_0.50)] w-full">
+    <article className="bg-white max-w-[20.4375rem] rounded-[0.625rem] shadow-[0_40px_40px_-10px_rgba(201_213_225_/_0.50)] w-full z-10">
       <ArticleCardHeader />
       <ArticleCardInformation />
       <ArticleCardFooter />
