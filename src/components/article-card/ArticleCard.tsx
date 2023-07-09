ea81f4800b12936356f43790c4823c4d93d47749
Ciensprog <jorg.leonrd@gmail.com>
2023-07-09 10:46:11 -0600

    feat: change element

--- src/components/article-card/ArticleCard.tsx
+++ src/components/article-card/ArticleCard.tsx
@@ -10,12 +10,12 @@ import { ArticleCardInformation } from './ArticleCardInformation'
 
 export function ArticleCard() {
   return (
-    <article className="bg-white max-w-[20.4375rem] rounded-[0.625rem] shadow-[0_40px_40px_-10px_rgba(201_213_225_/_0.50)] w-full z-10 md:flex md:max-w-[45.625rem]">
+    <section className="bg-white max-w-[20.4375rem] rounded-[0.625rem] shadow-[0_40px_40px_-10px_rgba(201_213_225_/_0.50)] w-full z-10 md:flex md:max-w-[45.625rem]">
       <ArticleCardHeader />
       <div className="md:flex md:flex-col md:px-2">
         <ArticleCardInformation />
         <ArticleCardFooter />
       </div>
-    </article>
+    </section>
   )
 }
