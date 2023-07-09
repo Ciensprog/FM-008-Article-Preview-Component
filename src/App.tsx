2ae8bab852b3b02a0b012a50f74af382ff0c3983
Ciensprog <jorg.leonrd@gmail.com>
2023-07-08 20:57:27 -0600

    feat: add footer card

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { IconShare } from '@/assets/icons/share'
+
 import { Attribution } from '@/components/Attribution'
 
 /*
@@ -16,7 +18,7 @@ export function App() {
               <img src="banner.jpg" alt="Article banner" />
             </picture>
           </header>
-          <section className="pb-8 pt-9 px-8">
+          <section className="pb-5 pt-9 px-8">
             <h1 className="font-bold leading-6 tracking-[0.0125rem]">
               Shift the overall look and feel by adding these wonderful touches
               to furniture in your home
@@ -27,6 +29,29 @@ export function App() {
               help you make any room feel complete.
             </p>
           </section>
+          <footer className="flex gap-5 items-center pb-5 pt-3 px-8">
+            <picture className="block h-10 w-10">
+              <img
+                src="profile.png"
+                className="h-full w-full"
+                alt="Profile picture for Michelle Appleton"
+              />
+            </picture>
+            <section className="">
+              <p className="font-bold leading-5 text-[0.8125rem] tracking-[0.0075rem]">
+                Michelle Appleton
+              </p>
+              <p className="font-medium leading-5 text-[0.8125rem] text-grey-very-light tracking-[0.0075rem]">
+                28 Jun 2020
+              </p>
+            </section>
+            <section className="ml-auto">
+              <button className="bg-default flex h-8 items-center rounded-full text-grey-light w-8">
+                <IconShare />
+                <span className="sr-only">Share article</span>
+              </button>
+            </section>
+          </footer>
         </article>
       </main>
 
