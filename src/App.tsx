2b89c095841f9f983b59f66a0927004991d6ddf3
Ciensprog <jorg.leonrd@gmail.com>
2023-07-08 20:24:56 -0600

    feat: add description

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,11 @@ export function App() {
               Shift the overall look and feel by adding these wonderful touches
               to furniture in your home
             </h1>
+            <p className="font-medium leading-5 mt-3 text-[0.8125rem] text-grey-light tracking-[0.0075rem]">
+              Ever been in a room and felt like something was missing? Perhaps
+              it felt slightly bare and uninviting. I’ve got some simple tips to
+              help you make any room feel complete.
+            </p>
           </section>
         </article>
       </main>
