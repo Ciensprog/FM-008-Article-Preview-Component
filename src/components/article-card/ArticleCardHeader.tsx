0809fb5e165fa5566d85dbd9d14161c6a95b6b89
Ciensprog <jorg.leonrd@gmail.com>
2023-07-09 11:00:05 -0600

    feat: update banner

--- src/components/article-card/ArticleCardHeader.tsx
+++ src/components/article-card/ArticleCardHeader.tsx
@@ -8,6 +8,7 @@ export function ArticleCardHeader() {
   return (
     <header className="flex-shrink-0 h-[12.5rem] overflow-hidden rounded-t-[0.625rem] md:h-[17.8125rem] md:rounded-bl-[0.625rem] md:rounded-tr-none md:w-[17.5rem]">
       <picture>
+        <source srcSet="banner-variation.jpg" media="(min-width: 768px)" />
         <img
           src="banner.jpg"
           className="h-full object-cover w-full"
